1f3d363ae3f21629127bdbded2729ff45b880d64
John Smith <johnsmithcoder1@gmail.com>
2024-02-15 21:22:12 -0600

    Add CollisionTypes enum

--- MarbleMaze/GameScene.swift
+++ MarbleMaze/GameScene.swift
@@ -7,6 +7,14 @@
 
 import SpriteKit
 
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
+
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
